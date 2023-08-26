4ec1a5b25978354ffc5dd76d629206a0c4977a2e
Gaurav Nayak <gauravgnayak.arya@gmail.com>
2023-08-26 23:02:17 +0530

    reame secret

--- list/main.tf
+++ list/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 
   subscription_id = "0fd8d403-ae7b-43e4-840a-5c4ff327c4cf"
   client_id       = "07b439ff-ef39-4b4c-b435-3522274e7c40"
-  client_secret   = "27U8Q~KTzoiiExk5eetJRrWbEbVAM2tltK7ifa6X"
+  client_secret   = "XXXX"
   tenant_id       = "e0cd89e2-e12a-4dd8-8ee8-2efb3946dc4f"
 }
 
